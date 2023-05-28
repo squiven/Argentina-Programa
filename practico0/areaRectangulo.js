cc5a1f838e0bbee6e3e19676b51e3efd59621ccf
squiven <squiven@gmail.com>
2023-05-28 00:01:14 -0300

    Practico N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -1,3 +1,12 @@
+// Escribir un programa JavaScript que calcule el área de un rectángulo. Deberá
+// guardar este programa en un archivo llamado: areaRectangulo.js. Luego,
+// responder a las siguientes preguntas utilizando su código para obtener los
+// resultados. Ayuda: la ecuación del área de un rectángulo está dada por:
+// 𝑎𝑟𝑒𝑎 = 𝑏𝑎𝑠𝑒 * 𝑎𝑙𝑡𝑢𝑟𝑎
+// a. ¿Cuál es el área de un rectángulo de base 5cm y altura 3cm?
+// b. ¿Cuál es el área de un rectángulo de base 12.5cm y altura 5.3cm?
+
+
 let base = 5;
 let altura = 3;
 
--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -1,3 +1,13 @@
+// Escribir un programa JavaScript que calcule el promedio de 3 números. Deberá
+// guardar este programa en un archivo llamado: promedio3.js. Luego, responder
+// a las siguientes preguntas utilizando su código para obtener los resultados. La
+// ecuación del promedio entre 3 números está dada por:
+// 𝑝𝑟𝑜𝑚𝑒𝑑𝑖𝑜 = (𝑥 + 𝑦 + 𝑧) / 33
+// a. ¿Cuál es el promedio entre 5, 7 y 9?
+// b. ¿Cuál es el promedio entre -2 , -3, -8?
+// c. ¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+
+
 let num1 = 5;
 let num2 = 7;
 let num3 = 9;
