013f7ef43e7bdd6cb9480974bee23282a3215a29
squiven <squiven@gmail.com>
2023-05-30 21:04:14 -0300

    Agrego el archivo conversionGrados.js

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -1,5 +1,5 @@
 // Escribir un programa JavaScript que realice la conversión de grados Fahrenheit
-// (F) a grados Celsius (C) . Deberá guardar este programa en un archivo llamado:
+// (F) a grados Celsius (C). Deberá guardar este programa en un archivo llamado:
 // conversionGrados.js. Luego, responder a las siguientes preguntas utilizando
 // su código para obtener los resultados. Ayuda: la ecuación de equivalencia está
 // dada por:
