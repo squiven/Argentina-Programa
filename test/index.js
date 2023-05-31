281bc7adc05f8fb55fe064e94b9f3b9c2d98b99e
squiven <squiven@gmail.com>
2023-05-30 21:28:35 -0300

    Editando

--- test/index.js
+++ test/index.js
@@ -1,2 +1,10 @@
-let entrada = prompt ("Ingrese su nombre");
-entrada = entrada.toLowerCase();
+let entrada = window.prompt ("Ingrese su nombre");
+let datos = entrada.split(" ");
+
+let nombre = document.getElementById("nombre");
+let apellido =document.getElementById("apellido");
+
+nombre.innerHTML = datos[0];
+apellido.innerHTML =datos[1];
+
+
