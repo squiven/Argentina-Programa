6abb2b57722aabf2dc96dc4bbe55f16f44cb8df4
Sebastián <squiven@gmail.com>
2023-06-22 19:19:59 -0300

    Practico N2 finalizado

--- practico2/mayorDeEdad.js
+++ practico2/mayorDeEdad.js
@@ -1,5 +1,5 @@
 // Escribe un programa para cada uno de los siguientes enunciados:
-// a. Solicite al usuario ingresar su edad. Si la edad es mayor o igual a 18,
+// Solicite al usuario ingresar su edad. Si la edad es mayor o igual a 18,
 // muestra por consola el mensaje: "Eres mayor de edad". De lo contrario,
 // muestra por consola el mensaje: "Eres menor de edad". Guardar en un
 // archivo llamado mayorDeEdad.js
